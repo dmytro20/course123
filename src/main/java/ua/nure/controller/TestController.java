93168c057477ea487d8f03b225f4e48f3ef7b90e
Dmytro Biletskyi <Dmytro_Biletskyi.com>
2017-03-14 18:03:30 +0200

    2 commit + template

--- src/main/java/ua/nure/controller/TestController.java
+++ src/main/java/ua/nure/controller/TestController.java
@@ -1,16 +1,11 @@
 package ua.nure.controller;
 
-import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringJoiner;
 
 @Controller
 public class TestController {
